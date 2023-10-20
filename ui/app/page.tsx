72c73627a0a1f97c5419235701c798c4f9e95d40
Darren Williams <apps@smartico.com>
2023-10-20 09:55:09 -0500

    rebase resolve

---
+++ ui/app/page.tsx
@@ -0,0 +1,11 @@
+// app/page.tsx
+import { nanoid } from '@/lib/utils'
+import { Chat } from '@/components/chat'
+
+export const runtime = 'edge'
+
+export default function IndexPage() {
+  const id = nanoid()
+
+  return <Chat id={id} />
+}
